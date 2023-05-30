ea89beab00752965822eba64314873e2296ca97d
Caio <108128119+DocCaio@users.noreply.github.com>
2023-05-30 09:39:28 -0300

    teste

---
+++ programa.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 7;
+    int b =2;
+    int c = a + b;
+
+    if (c != 0) {
+        c++;
+    }
+
+    printf("olá, c = %d\n" , c);
+
+return 0;
+
+}
+
